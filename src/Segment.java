68c986d766a2b284bf632584839a6469472a9a07
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2022-06-27 12:53:18 +0200

    Changed Segmnent copying constructor to deep copy it's contents.

--- src/Segment.java
+++ src/Segment.java
@@ -5,13 +5,13 @@ public class Segment implements Cloneable {
         if (a.equals(b)) {
             throw new Exception("Points must differ.");
         }
-        _a = a;
-        _b = b;
+        _a = new Point(a);
+        _b = new Point(b);
     }
 
     Segment(Segment other) {
-        _a = other._a;
-        _b = other._b;
+        _a = new Point(other._a);
+        _b = new Point(other._b);
     }
 
     public Point getA() {
@@ -25,13 +25,13 @@ public class Segment implements Cloneable {
     public void setA(Point a) throws Exception {
         if (a.equals(_b))
             throw new Exception("New point A must differ from point B.");
-        _a = a;
+        _a = new Point(a);
     }
 
     public void setB(Point b) throws Exception {
         if (b.equals(_a))
             throw new Exception("New point B must differ from point A.");
-        _b = b;
+        _b = new Point(b);
     }
 
     public double getLength() {
