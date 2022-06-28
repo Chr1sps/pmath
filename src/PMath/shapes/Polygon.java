04e81cd4f6a1f440fbe21a83b0ce03f9821c066b
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2022-06-28 02:43:13 +0200

    Refactored Polygon class to use PMath.utils algorithms instead of internal ones.

--- src/PMath/shapes/Polygon.java
+++ src/PMath/shapes/Polygon.java
@@ -6,50 +6,10 @@ import PMath.exceptions.ColinearVerticesException;
 import PMath.exceptions.IdenticalVerticesException;
 import PMath.exceptions.InsufficientVerticesException;
 import PMath.exceptions.IntersectingEdgesException;
+import PMath.utils;
 
 public class Polygon implements Cloneable {
     private ArrayList<Point> _vertices;
-
-    private double _determinant(Point a, Point b, Point c) {
-        return ((a.getX() - c.getX()) * (b.getY() - a.getY())) - ((a.getX() - b.getX()) * (c.getY() - a.getY()));
-    }
-
-    private double _determinant(Point p, Segment s) {
-        return _determinant(p, s.getA(), s.getB());
-    }
-
-    private boolean _isAdherent(Point p, Segment s) {
-        if (_determinant(p, s) == 0.0) {
-            Point a = s.getA(), b = s.getB();
-            if (b.getX() - a.getX() == 0.0) {
-                if (p.getY() >= Math.min(a.getY(), b.getY()) && p.getY() <= Math.max(a.getY(), b.getY()))
-                    return true;
-            } else {
-                if (p.getX() >= Math.min(a.getX(), b.getX()) && p.getX() <= Math.max(a.getX(), b.getX()))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean _areIntersected(Segment first, Segment other) {
-        double det_1 = _determinant(first.getA(), other) * _determinant(first.getB(), other),
-                det_2 = _determinant(other.getA(), first) * _determinant(other.getB(), first);
-        if (det_1 < 0 && det_2 < 0)
-            return true;
-        else if (det_1 == 0 && det_2 < 0) {
-            if (_isAdherent(first.getA(), other) || _isAdherent(first.getB(), other))
-                return true;
-        } else if (det_1 < 0 && det_2 == 0) {
-            if (_isAdherent(other.getA(), first) || _isAdherent(other.getB(), first))
-                return true;
-        } else if (det_1 == 0 && det_2 == 0) {
-            if ((_isAdherent(first.getA(), other) || _isAdherent(first.getB(), other))
-                    && (_isAdherent(other.getA(), first) || _isAdherent(other.getB(), first)))
-                return true;
-        }
-        return false;
-    }
 
     public Polygon(Point[] vertices)
             throws InsufficientVerticesException,
@@ -66,7 +26,7 @@ public class Polygon implements Cloneable {
         Point pprev = vertices[vertices.length - 2], prev = vertices[vertices.length - 1];
         for (Point current : vertices) {
 
-            if (_determinant(current, prev, pprev) == 0)
+            if (utils.determinant(current, prev, pprev) == 0)
                 throw new ColinearVerticesException("No 3 subsequent points shall lie on the same line.");
             pprev = prev;
             prev = current;
@@ -90,7 +50,7 @@ public class Polygon implements Cloneable {
                 offset = 0;
 
             for (int j = i + 2; j < sides.size() - offset; ++j) {
-                if (_areIntersected(sides.get(i), sides.get(j)))
+                if (utils.areIntersected(sides.get(i), sides.get(j)))
                     throw new IntersectingEdgesException("No two edges shall intersect with each other.");
 
             }
@@ -144,12 +104,12 @@ public class Polygon implements Cloneable {
             Point pprev = new Point(_vertices.get(_vertices.size() - 2)),
                     prev = new Point(_vertices.get(_vertices.size() - 1));
             boolean more_than;
-            if (_determinant(_vertices.get(0), prev, pprev) > 0)
+            if (utils.determinant(_vertices.get(0), prev, pprev) > 0)
                 more_than = true;
             else
                 more_than = false;
             for (int i = 1; i < _vertices.size(); ++i) {
-                if (!(_determinant(_vertices.get(i), prev, pprev) < 0 ^ more_than))
+                if (!(utils.determinant(_vertices.get(i), prev, pprev) < 0 ^ more_than))
                     return true;
                 pprev = prev;
                 prev = _vertices.get(i);
