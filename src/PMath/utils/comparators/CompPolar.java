db96ac9ac2c5dab1534c5d0adce12c13864f3ab2
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2022-07-13 20:00:25 +0200

    Removed unused import in CompPolar.

--- src/PMath/utils/comparators/CompPolar.java
+++ src/PMath/utils/comparators/CompPolar.java
@@ -2,7 +2,6 @@ package PMath.utils.comparators;
 
 import java.util.Comparator;
 
-import PMath.exceptions.IdenticalVerticesException;
 import PMath.exceptions.OriginPointException;
 import PMath.shapes.Point;
 import PMath.utils.algorithms;
